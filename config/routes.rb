f5dd646ea8dc40215e877c07dfe7f7c1bd821c7b
hokutonokenn <108508259+hokutonokenn@users.noreply.github.com>
2022-09-02 06:21:04 +0000

    [Finish]task1-1

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books, only: [:index, :show, :edit]
+  resources :users, only: [:show, :edit]
+
 end
