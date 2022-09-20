2a89a704dc8a7c7b741f46784829e6f8d2d47ee1
hokutonokenn <108508259+hokutonokenn@users.noreply.github.com>
2022-09-20 13:29:56 +0000

    [Task]3-4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,36 @@
 class BooksController < ApplicationController
+
+
+  def create
+    @book = Book.new(book_params)
+
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books=Book.all
+      @user = current_user
+      render :index
+    end
+  end
+
   def index
+    @books=Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def show
   end
 
   def edit
+    if current_user
+      @book = Book.find(params[:id])
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
