25061ea26acbce8714105f68d357447a0d1bf8ab
hokutonokenn <108508259+hokutonokenn@users.noreply.github.com>
2022-09-22 05:41:22 +0000

    [Task]3-5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,6 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-
     @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book.id)
@@ -21,6 +20,9 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
   end
 
   def edit
@@ -28,6 +30,22 @@ class BooksController < ApplicationController
       @book = Book.find(params[:id])
     end
   end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+        redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
 
   private
   def book_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
   def edit
      @user = User.find(params[:id])
-    if @user != current_user
+    if @user == current_user
       render "edit"
     else
       redirect_to user_path(current_user.id)
